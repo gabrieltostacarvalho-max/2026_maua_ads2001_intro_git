289af0999a7d0d97cb8b3721c67d3c7f5b4d2939
Gabriel tosta Carvalho <gabrieltostacarvalho@gmail.com>
2026-08-12 21:54:20 -0300

    feat: executa menu e adiciona opção sair

--- menu.py
+++ menu.py
@@ -1,27 +1,33 @@
 def menu():
-    print("1. Somar")
-    print("2. Subtrair")
-    print("3. Multiplicar")
-    print("4. Dividir")
-    print("0. Sair")
-     
-    opcao = input("Escolha uma opção: ")
-    if opcao == "1":
-        a = float(input("Digite o primeiro número: "))
-        b = float(input("Digite o segundo número: "))
-        print("Resultado:", a + b)
-        
-    elif opcao == "2":
-        a = float(input("Digite o primeiro número: "))
-        b = float(input("Digite o segundo número: "))
-        print("Resultado:", a - b)
-        
-    elif opcao == "3":
-        a = float(input("Digite o primeiro número: "))
-        b = float(input("Digite o segundo número: "))
-        print("Resultado:", a * b)
-    
-    elif opcao == "4":
-        a = float(input("Digite o primeiro número: "))
-        b = float(input("Digite o segundo número: "))
-        print("Resultado:", a / b)
+    while True:
+        print("1. Somar")
+        print("2. Subtrair")
+        print("3. Multiplicar")
+        print("4. Dividir")
+        print("0. Sair")
+
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            a = float(input("Digite o primeiro número: "))
+            b = float(input("Digite o segundo número: "))
+            print("Resultado:", a + b)
+
+        elif opcao == "2":
+            a = float(input("Digite o primeiro número: "))
+            b = float(input("Digite o segundo número: "))
+            print("Resultado:", a - b)
+
+        elif opcao == "3":
+            a = float(input("Digite o primeiro número: "))
+            b = float(input("Digite o segundo número: "))
+            print("Resultado:", a * b)
+
+        elif opcao == "4":
+            a = float(input("Digite o primeiro número: "))
+            b = float(input("Digite o segundo número: "))
+            print("Resultado:", a / b)
+
+        elif opcao == "0":
+            print("Saindo...")
+            break
